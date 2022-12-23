e002cb1cc3df1af737d392258aea07c3c94df5ef
NormalRandomDude <110547057+NormalRandomDude@users.noreply.github.com>
2022-12-23 19:12:03 +0700

    Update bss.lua

--- bss.lua
+++ bss.lua
@@ -2950,10 +2950,10 @@ local themes = guisettings:CreateDropdown("Image", {
 end)
 themes:SetOption("Default")
 local kocmocs = setttab:CreateSection("Configs")
-kocmocs:CreateTextBox("Config Name", "eg: redhive", false, function() temptable.configname = game.Players.LocalPlayer.Name end)
+kocmocs:CreateTextBox("Config Name", "eg: redhive", false, function(Value) temptable.configname = game.Players.LocalPlayer.Name end)
 kocmocs:CreateButton("Load Config", function()
     if not isfile("kocmoc/BSS_" .. temptable.configname .. ".json") then
-        api.notify("Rosemoc " .. temptable.version, "No such config file!", 2)
+        api.notify("Rosepog " .. temptable.version, "No such config file!", 2)
     else
         kocmoc = game:service("HttpService"):JSONDecode(readfile("kocmoc/BSS_" .. temptable.configname .. ".json"))
         for i,v in pairs(guiElements) do
